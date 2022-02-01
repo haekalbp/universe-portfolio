b36b16d348e5eb17bba2a5bd732a16e9e1de807b
Haekal BP <93190990+haekalbp@users.noreply.github.com>
2022-02-01 14:28:34 +0700

    Form

---
+++ js/validate.js
@@ -0,0 +1,8 @@
+// const name = document.getElementById('name')
+// const email = document.getElementById('email')
+// const message = document.getElementById('message')
+// const form = document.getElementById('form')
+
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault()
+// })
